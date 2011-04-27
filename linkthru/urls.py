4d009804b1b83d7d52cb496d4cc4b689ffa33e64
Colin Powell <colin@onecardinal.com>
2011-04-27 14:24:53 -0400

    Fixing bug in urls.

--- linkthru/urls.py
+++ linkthru/urls.py
@@ -4,8 +4,8 @@ from linkthru.views import linkthru_view, linkthru_detail
 
 urlpatterns = patterns('',
     url(r'^view/(?P<id>[\d]+)/$', linkthru_view, name='lt-linkthru-view'),
+    url(r'^linkthrus/(?P<id>[\d]+)/$', linkthru_detail, name='lt-linkthru-detail'),
     #url(r'^addisplay.html', ad_display, name='adzone_ad_display'),
     #url(r'^linkthrus/$', linkthru_index, name='lt-index'),
-    url(r'^linkthrus/<?P<id>[\d]+)/$', linkthru_detail, name='lt-linkthru-detail'),
     #url(r'^xhr_ads/(?P<format>\w+)$', xhr_ad_table, name='adzone_xhr_ad_table'),
 )
