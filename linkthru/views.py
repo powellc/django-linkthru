493f6eb5ddda47db50a2368337e35467c0651a1d
Colin Powell <colin@onecardinal.com>
2011-04-27 14:28:22 -0400

    Fix error in view.

--- linkthru/views.py
+++ linkthru/views.py
@@ -14,7 +14,7 @@ def linkthru_view(request, id):
     Record the click in the database, then redirect to the thru url
 
     """
-    link = get_object_or_404(LinkBase, id=id)
+    link = get_object_or_404(LinkThruBase, id=id)
     logging.debug('Found ad: %s, redirecting...' % (ad))
     try:
         click = LinkClick(ad=ad, click_date=datetime.now(), source_ip=request.META.get('REMOTE_ADDR'))
@@ -25,7 +25,7 @@ def linkthru_view(request, id):
 
 @staff_member_required
 def linkthru_detail(request, id):
-    link = get_object_or_404(LinkBase, id=id)
+    link = get_object_or_404(LinkThruBase, id=id)
     return render_to_response('linkthru/linkthru_detail.html', locals(),
                               context_instance=RequestContext(request))
 
